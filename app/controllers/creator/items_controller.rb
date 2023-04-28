f09a924dc292c14ef2cb1750985e46736141f1c4
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-04-28 13:00:56 +0000

    [creator.item.new:23.04.28]

--- app/controllers/creator/items_controller.rb
+++ app/controllers/creator/items_controller.rb
@@ -1,6 +1,38 @@
 class Creator::ItemsController < ApplicationController
 
+  def new
+    @item = Item.new
+    @colors = Color.all
+    @sizes = Size.all
+    @genres = Genre.all
+    @styles = Style.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to creator_items_path
+  end
+
   def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+private
+  def item_params
+    params.require(:item).permit(:creator_id, :color_id, :size_id, :genre_id, :style_id, :name, :introduction, :price, :is_active)
   end
 
 end
