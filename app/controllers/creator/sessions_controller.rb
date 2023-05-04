3673c2512842dafc6dba82ddda8893eb2ff2e66b
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-05-04 16:24:56 +0000

    [is_deleted:23.05.04]

--- app/controllers/creator/sessions_controller.rb
+++ app/controllers/creator/sessions_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class Creator::SessionsController < Devise::SessionsController
+
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :creator_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -24,4 +26,17 @@ class Creator::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+
+protected
+  def creator_state
+    @creator = Creator.find_by(email: params[:creator][:email])
+    return if !@creator
+    if @creator.valid_password?(params[:creator][:password])
+      if @creator.is_deleted == true
+        redirect_to new_creator_registration_path
+      end
+    end
+  end
+
 end
--- app/controllers/customer/sessions_controller.rb
+++ app/controllers/customer/sessions_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class Customer::SessionsController < Devise::SessionsController
+
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -24,4 +26,17 @@ class Customer::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+
+protected
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    if @customer.valid_password?(params[:customer][:password])
+      if @customer.is_deleted == true
+        redirect_to new_customer_registration_path
+      end
+    end
+  end
+
 end
