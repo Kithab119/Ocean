b68d5a543b8053e264d8fea2a8a4fe85f24c92bc
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-05-13 16:28:47 +0000

    [layout.customer.sign:23.05.13]

--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -1,6 +1,8 @@
 class Customer::HomesController < ApplicationController
 
   def top
+    @items = Item.all
+    @creators = Creator.all
     @colors = Color.all
     @sizes = Size.all
     @genres = Genre.all
