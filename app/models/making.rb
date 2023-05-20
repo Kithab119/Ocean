4b6ceca56c67e0d5e17f66cfcd895e57ea9f364f
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-05-20 10:13:12 +0000

    [layout.customer:23.05.20]

--- app/models/making.rb
+++ app/models/making.rb
@@ -3,6 +3,6 @@ class Making < ApplicationRecord
   belongs_to :customer
   belongs_to :creator
 
-  enum is_product: {waiting: 0, product: 1, complete: 2}
+  enum is_product: {confirm: 0,approved: 1,product_waiting: 2, product: 3, complete: 4}
 
 end
